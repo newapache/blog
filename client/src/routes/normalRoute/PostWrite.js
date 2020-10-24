a45093d1584b6b0270d15c295d661805b75e0ff0
jang-yunmi <newapache@naver.com>
2020-10-24 23:00:47 +0900

    postwrite ckeditor s3

---
+++ client/src/routes/normalRoute/PostWrite.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Col,
+  Progress,
+} from "reactstrap";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import { editorConfiguration } from "../../components/editor/EditorConfig";
+import Myinit from "../../components/editor/UploadAdapter";
+import { POST_UPLOADING_REQUEST } from "../../redux/types";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PostWrite = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const [form, setValues] = useState({ title: "", contents: "", fileUrl: "" });
+  const dispatch = useDispatch();
+
+  const onSubmit = async (e) => {
+    await e.preventDefault();
+    const { title, contents, fileUrl, category } = form;
+    const token = localStorage.getItem("token");
+    const body = { title, contents, fileUrl, category, token };
+    dispatch({
+      type: POST_UPLOADING_REQUEST,
+      payload: body,
+    });
+  };
+
+  const onChange = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const getDataFromCKEditor = () => {
+    console.log("ee");
+  };
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <Form onSubmit={onSubmit}>
+          <FormGroup className="mb-3">
+            <Label for="title">Title</Label>
+            <Input
+              type="text"
+              name="title"
+              id="title"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="category">Category</Label>
+            <Input
+              type="text"
+              name="category"
+              id="category"
+              className="form-control"
+              onChange={onChange}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Label for="content">Content</Label>
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfiguration}
+              onInit={Myinit}
+              onBlur={getDataFromCKEditor}
+            />
+            <Button
+              color="success"
+              block
+              className="mt-3 col-md-2 offset-md-10 mb-3"
+            >
+              제출하기
+            </Button>
+          </FormGroup>
+        </Form>
+      ) : (
+        <Col width={50} className="p-5 m-5">
+          <Progress animated color="info" value={100} />
+        </Col>
+      )}
+    </div>
+  );
+};
+
+export default PostWrite;
